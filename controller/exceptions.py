48ad7d8b382e00b0085b907cf980747c112df367
Antonió <antonio@tomosev.dev>
2023-04-20 20:52:40 +0100

    removed

--- controller/exceptions.py
+++
@@ -1,6 +0,0 @@
-
-class AcquireLockError(Exception):
-    """Exception raised for lock unable to be acquired"""
-    def __init__(self):
-        self.message = "Unable to acquire task lock, a task using the same key is already running. Please try again."
-        super().__init__(self.message)

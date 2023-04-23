c50ee309f01e14357d182e44de8b285850133f2b
Antonió <antonio@tomosev.dev>
2023-04-23 12:41:13 +0100

    created logger.py

---
+++ controller/metrics/logger.py
@@ -0,0 +1,29 @@
+"""
+logger.py
+"""
+import logging
+from controller import settings
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+console = logging.StreamHandler()
+
+file = logging.handlers.RotatingFileHandler(
+    filename="logs/netorc.log", mode="w", maxBytes=10000000, backupCount=5
+)
+
+syslog = logging.handlers.SysLogHandler(
+    address=(settings.SYSLOG_SERVER, settings.SYSLOG_PORT)
+)
+
+formatter = logging.Formatter(settings.LOG_FORMAT)
+
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+file.setFormatter(formatter)
+logger.addHandler(file)
+
+syslog.setFormatter(formatter)
+logger.addHandler(syslog)

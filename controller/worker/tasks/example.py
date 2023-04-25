e02818597ec80aa20138f7325282a48e0bb3bf69
Antonió <antonio@tomosev.dev>
2023-04-25 08:55:03 +0100

    updated task examples

--- controller/worker/tasks/example.py
+++ controller/worker/tasks/example.py
@@ -1,26 +1,26 @@
 """
 example.py
 
-`sync_lock` can be used to ensure tasks are executed
+`task_lock` can be used to ensure tasks are executed
 synchronously with each worker obtaining the lock before 
 executing the task. 
 
 for more information, see <link>
 
 @celery.task()
-@sync_lock
-def a_sync_lock_task() -> str:
-    print("Starting a task with sync_lock...")
+@task_lock
+def a_task_lock_task() -> None:
+    print("Starting a task with task_lock...")
     time.sleep(10)
-    print("Finished task with sync_lock.")
+    print("Finished task with task_lock.")
 
 
 @celery.task()
-@sync_lock
-def a_sync_lock_task(sync_lock_key: str = None) -> str:
-    print(f"Starting task: {sync_lock_key}, with sync_lock...")
+@task_lock
+def a_task_lock_task(task_lock_key: str = None) -> None:
+    print(f"Starting task: {task_lock_key}, with task_lock...")
     time.sleep(10)
-    print(f"Finished task: {sync_lock_key}, with sync_lock.")
+    print(f"Finished task: {task_lock_key}, with task_lock.")
 
 """
 import time
@@ -30,7 +30,7 @@ from controller.misc.decorators import task_lock
 
 @celery.task()
 @task_lock
-def example_task() -> str:
+def example_task() -> None:
     print("Starting task")
     time.sleep(2)
     print("Finished task")

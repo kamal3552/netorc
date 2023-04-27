ff3ad0d4f70bb8e19c0fefa300afd85741e3fd16
Antonió <antonio@tomosev.dev>
2023-04-27 20:15:54 +0100

    updated exception msg

--- controller/misc/exceptions.py
+++ controller/misc/exceptions.py
@@ -2,5 +2,7 @@ class AddLockException(Exception):
     """Exception raised for lock not aquired"""
 
     def __init__(self):
-        self.message = "Unable to acquire task lock, a task using the same key is already running. Please try again."
+        self.message = (
+            "Unable to add lock to task. A task using the same key is running."
+        )
         super().__init__(self.message)
